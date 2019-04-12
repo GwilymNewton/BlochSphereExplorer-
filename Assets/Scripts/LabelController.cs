6467fec97fc575e12d391f47f45af3f535f69fb2
Gwilym Newton <gwilnew@uk.ibm.com>
2019-04-12 11:08:14 +0100

    Beter labeling

---
+++ Assets/Scripts/LabelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelController : MonoBehaviour {
+
+    public GameObject qbit;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - qbit.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = qbit.transform.position + offset;
+    }
+}
